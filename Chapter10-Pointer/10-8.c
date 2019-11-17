4996fd4a80e0e973a7ec7dc29e9f72005011365c
plushugh <hughhji@outlook.com>
2019-11-17 13:04:35 +0800

    10-8

---
+++ Chapter10-Pointer/10-8.c
@@ -0,0 +1,26 @@
+//Wrong example
+#include <stdio.h>
+
+void swap(int *px, int *py)
+{
+    int temp = *px;
+    *px = *py;
+    *py = temp;
+}
+
+int main(void)
+{
+    double da, db;
+
+    puts("Enter two real numbers:");
+    printf("A:");   scanf("%1f", &da);
+    printf("B:");   scanf("%1f", &db);
+
+    swap(&da, &db);
+
+    puts("Swapped the two numbers.");
+    printf("integer A:%f\n",da);
+    printf("integer B:%f\n",db);
+
+    return 0
+}
