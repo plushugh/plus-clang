852ddf03d2ce3913453cec5f35b0617d29d07b66
plushugh <www.linuxforum.net@gmail.com>
2019-12-30 19:11:12 +0800

    10-10

---
+++ Chapter10-Pointer/10-10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int a[5] = {1, 2, 3, 4, 5};
+    int *p = a;
+
+    for (i = 0; i < 5; i++)
+        printf("a[%d] = %d  *(a+%d) = %d  p[%d] = %d  *(p+%d) = %d\n",
+        i, a[i], i, *(a + i), i, p[i], i, *(p + i));
+    for (i = 0; i < 5; i++)
+        printf("&a[%d] = %p  a+%d = %p  &p[%d] = %p  p+%d = %p\n",
+        i, &a[i], i, (a + i), i, &p[i], i, (p + i));
+    return 0;
+}
