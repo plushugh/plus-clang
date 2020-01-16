14b7339481488868d401d43e227eb3f436617001
plushugh <hughhji@outlook.com>
2020-01-16 20:50:44 +0800

    4-15

---
+++ Chapter4-Loop/4-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, n;
+
+    printf("Integer:");
+    scanf("%d", &n);
+
+    for (i=1; 1<=1;i++) {
+        if (n % i == 0)
+            printf("%d ", i);
+    }
+    putchar("\n");
+
+    return 0;
+}
