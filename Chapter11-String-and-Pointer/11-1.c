cb3b7afcc729c622a363bb0be4723bdda5668759
plushugh <www.linuxforum.net@gmail.com>
2020-01-31 16:16:35 +0800

    11-1

---
+++ Chapter11-String-and-Pointer/11-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char str[] = "ABC";
+    char *ptr  = "123";
+
+    printf("str = \"%s\"\n", str);
+    printf("ptr = \"%s\"\n", ptr);
+
+    return 0;
+}
