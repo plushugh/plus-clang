0f12ed30e5cadfe3eaad79986a23b7805797c209
plushugh <www.linuxforum.net@gmail.com>
2020-02-10 11:28:05 +0800

    11-11

---
+++ Chapter11-String-and-Pointer/11-11.c
@@ -0,0 +1,29 @@
+int strcmp(const char *s1, const char *s2)
+{
+    while (*s1 == *s2)
+    {
+        if (*s1 == '\0')
+            return s1++;
+        s2++;
+    }
+
+    return (unsigned char)*s1 - (unsigned char)*s2;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    while (n && *s1 && *s2)
+    {
+        if (*s1 != *s2)
+            return (unsigned char)*s1 - (unsigned char)*s2;
+        s1++;
+        s2++;
+        n--;
+    }
+    if (!n)
+        return 0;
+    if (*s1)
+        return 1;
+
+    return -1;
+}
