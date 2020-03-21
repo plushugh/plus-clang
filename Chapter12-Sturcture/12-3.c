d12345bf263ef0cffb21bde5121454498a4f172b
Hugh He <plushugh@gmail.com>
2020-03-21 19:31:28 +0800

    fix 12-3

--- Chapter12-Sturcture/12-3.c
+++ Chapter12-Sturcture/12-3.c
@@ -6,20 +6,20 @@
 
 struct student
 {
-	char  name[NAME_LEN];
-	int   height;
+	char name[NAME_LEN];
+	int height;
 	float weight;
-	long  schols;
+	long schols;
 };
 
 int main(void)
 {
 	struct student takao = {"Takao", 173, 86.2};
 
-	printf("name = %s\n",   sanaka.name);
-	printf("height = %d\n", sanaka.height);
-	printf("weight = %.1f\n", sanaka.weight);
-	printf("schols = %1d\n", sanaka.schols);
+	printf("name = %s\n", takao.name);
+	printf("height = %d\n", takao.height);
+	printf("weight = %.1f\n", takao.weight);
+	printf("schols = %1d\n", takao.schols);
 
 	return 0;
 }
