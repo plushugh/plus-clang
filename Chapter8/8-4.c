a9a96aae86b6c53fbd86f663edad7bad9a2d2790
Hugh He <plushugh@gmail.com>
2020-05-17 12:00:18 +0800

    8-4

---
+++ Chapter8/8-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define puts_alert(str) (putchar('\a'), puts(str))
+
+int main(void)
+{
+	int n;
+
+	printf("Enter an integer");
+	scanf("%d", &n);
+
+	if (n)
+		puts_alert("This number is not 0");
+	else
+		puts_alert("This number is 0");
+
+	return 0;
+}
