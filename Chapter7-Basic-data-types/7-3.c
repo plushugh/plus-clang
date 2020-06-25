ad1d45a75cb5ced8ff62b3cb84e9df45a26c0bf1
Hugh He <plushugh@gmail.com>
2020-06-25 12:17:36 +0800

    update filename

--- Chapter7-Basic-data-types/7-3.c
+++ Chapter7-Basic-data-types/7-3.c
@@ -5,7 +5,7 @@ int main(void)
 	printf("sizeof(char)  = %u\n", (unsigned)sizeof(char));
 	printf("sizeof(short) = %u\n", (unsigned)sizeof(short));
 	printf("sizeof(int)   = %u\n", (unsigned)sizeof(int));
-	printf("sizeof(long)  = %u\n", (unsigned)sizeof(long));
+	printf("sizeof(long)  = %u\n", (unsigned)sizeof(long)
 
 	return 0;
 }
