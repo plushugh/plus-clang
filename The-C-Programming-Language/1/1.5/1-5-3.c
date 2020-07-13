531caae0a658cc0e84d3ab8d3ab93d20ff73da99
Hugh He <plushugh@gmail.com>
2020-07-13 11:38:11 +0800

    1-5-3

---
+++ The-C-Programming-Language/1/1.5/1-5-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int c, n1;
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++n1
+                printf("%d\n", n1)
+}
