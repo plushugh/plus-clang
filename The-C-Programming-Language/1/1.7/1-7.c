1991f203ca876071d6780b367e42ffcf780b9b76
Hugh He <plushugh@gmail.com>
2020-07-15 11:46:44 +0800

    1.7

---
+++ The-C-Programming-Language/1/1.7/1-7.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+inp power(int m, int n);
+
+int main()
+{
+    int i;
+
+    for (i = 0; i < 100; ++i)
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
+    return 0;
+}
+
+int power(int base, int n)
+{
+    int i, p;
+
+    p = 1;
+    for (i = 1; i <= n; ++i)
+        p = p * base;
+    return p;
+}
