44dfc775f3bbd4f581ed1e55700d382b5931fcb5
Hugh He <plushugh@gmail.com>
2020-07-19 10:46:06 +0800

    3.3

---
+++ The-C-Programming-Language/3/3-3.c
@@ -0,0 +1,18 @@
+int binsearch(int x, int v[], int n)
+{
+    int low, mid, high;
+
+    low = 0,
+    high = n - 1;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
