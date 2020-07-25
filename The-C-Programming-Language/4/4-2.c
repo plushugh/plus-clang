a8a2e08606de02f4b3f85b05517c6c9c47f78edc
Hugh He <plushugh@gmail.com>
2020-07-25 11:31:47 +0800

    merge files

---
+++ The-C-Programming-Language/4/4-2.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAXLINE 100
+
+
+
+int gline(char line[], int max);
+int strindex(char source[], char searchfor[]);
+
+
+int main()
+{
+    double sum, atof(char[]);
+    char line[MAXLINE];
+    int gline(char line[], int max);
+
+    sum = 0;
+    while(gline(line,MAXLINE)>0)
+        printf("\t%g\n", sum += atof(line));
+    return 0;
+}
+
+int gline(char s[], int lim)
+{
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+int strindex(char s[], char t[])
+{
+    int i, j, k;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = i, k = 0; t[k] != '0' && s[j] == t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
+
+double atof(char s[])
+{
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if(s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if(s[i]=='.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
