1475aa29de7afc7b02194a4346efa30142e8e2b2
Hugh He <plushugh@gmail.com>
2020-07-27 11:24:23 +0800

    pointer-1

---
+++ The-C-Programming-Language/5/pointer-string-opeartions.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int strl(char *s)
+{
+    int n;
+
+    for (n = 0; *s != '\0';s++)
+        n++;
+    return n;
+}
+void strc(char *s, char *t)
+{
+    while(*s++ = *t++)
+        ;
+}
+int strcm(char *s, char *t)
+{
+    for (; *s == *t; s++, t++)
+        if(*s == '\0')
+            return 0;
+    return *s - *t;
+}
+
+int main()
+{
+    char str[20] = "ExampleString";
+    char str2[20] = "asd";
+    printf("%d\n", strl(str));
+    for ( int i = 0; i < 10; i++)
+    {
+    
+    strc(str2, str);
+    printf("str2: %s\n", str2);
+    }
+}
