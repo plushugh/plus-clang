fe8615a4659c17265344f4529a4c1d279a003984
Hugh He <plushugh@gmail.com>
2020-07-31 13:30:10 +0800

    mylib

---
+++ The-C-Programming-Language/5/mylib.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ALLOCSIZE 10000
+#ifndef MAXLEN
+#define MAXLEN 1000
+#endif
+
+void swap(int v[], int i, int j);
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n)
+    {
+        allocp += n;
+        return allocp - n;
+    }
+    else
+        return 0;
+}
+void afree(char *p)
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
+
+int gline(char s[], int lim)
+{
+    int c, i;
+    i = 0;
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
+        s[i++] = c;
+    if (c == '\n')
+        s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
+
+int stridx(char s[], char t[])
+{
+    int i, j, k;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = i, k = 0; t[k] != '0' && s[j] == t[k]; j++, k++)
+            ;
+        if (k > 0 && t[k] == '\0')
+            return i;
+    }
+    return -1;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0)
+        printf("%s\n", *lineptr++);
+}
+
+int readlines(char *lineptr[], int maxlines)
+{
+    int len, nlines;
+    char *p, line[MAXLEN];
+    nlines = 0;
+    while ((len = gline(line, MAXLEN)) > 0)
+        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+            return -1;
+        else
+        {
+            line[len - 1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    return nlines;
+}
+
+void nmlqsort(int v[], int left, int right)
+{
+    int i, last;
+
+    if (left >= right)
+        return;
+    swap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if (v[i] < v[left])
+            swap(v, ++last, i);
+    swap(v, left, last);
+    nmlqsort(v, left, last - 1);
+    nmlqsort(v, last + 1, right);
+}
+
+void ptrqsort(void *v[], int left, int right, int (*comp)(void *, void *))
+{
+    int i, last;
+    void ptrswap(void *v[], int, int);
+
+    if (left >= right)
+        return;
+    ptrswap(v, left, (left + right) / 2);
+    last = left;
+    for (i = left + 1; i <= right; i++)
+        if ((*comp)(v[i], v[left]) < 0)
+            ptrswap(v, ++last, i);
+    ptrswap(v, left, last);
+    ptrqsort(v, left, last - 1, comp);
+    ptrqsort(v, left + 1, right, comp);
+}
+
+int numcmp(char *s1, char *s2)
+{
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
+}
+
+void ptrswap(void *v[], int i, int j)
+{
+    int *tmp;
+    tmp = v[i];
+    v[i] = v[j];
+    v[j] = tmp;
+}
+
+void swap(int v[], int i, int j)
+{
+    int tmp;
+    tmp = v[i];
+    v[i] = v[j];
+    v[j] = tmp;
+}
