5d4199242e5e6be968bba20777872e4f8f2d2792
Hugh He <plushugh@gmail.com>
2020-08-05 11:50:00 +0800

    nlist

---
+++ The-C-Programming-Language/6/nlist.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdlib.h>
+
+struct nlist
+{
+  struct nlist *next;
+  char *name;
+  char *defn;
+};
+
+#define HASHSIZE 101
+
+static struct nlist *hashtab[HASHSIZE];
+
+unsigned hash(char *s)
+{
+  unsigned hashval;
+
+  for (hashval = 0; *s != '\0'; s++)
+    hashval = *s + 31 * hashval;
+  return hashval % HASHSIZE;
+}
+
+struct nlist *lookup(char *s)
+{
+  struct nlist *np;
+
+  for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+    if (strcmp(s, np->name) == 0)
+      return np;
+  return NULL;
+}
+
+struct nlist *lookup(char *);
+
+struct nlist *install(char *name, char *defn)
+{
+  struct nlist *np;
+  unsigned hashval;
+  if ((np = lookup(name)) == NULL)
+  {
+    np = (struct nlist *)malloc(sizeof(*np));
+    if (np = NULL || (np->name = strdup(name)) == NULL)
+      return NULL;
+    hashval = hash(name);
+    np->next = hashtab[hashval];
+    hashtab[hashval] = np;
+  }
+  else
+    free((void *)np->defn);
+  if ((np->defn = strdup(defn)) == NULL)
+    return NULL;
+  return np;
+}
