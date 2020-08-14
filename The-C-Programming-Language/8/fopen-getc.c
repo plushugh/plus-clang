29db2f993ecff07047387fb82ceac8e6767090ca
Hugh He <plushugh@gmail.com>
2020-08-14 11:59:43 +0800

    fopen + fillbuf

--- The-C-Programming-Language/8/fopen-getc.c
+++ The-C-Programming-Language/8/fopen-getc.c
@@ -42,3 +42,66 @@ int _flushbuf(int, FILE *);
 
 #define getchar() getc(stdin)
 #define putchar(x) putc((x), stdout)
+
+#include <unistd.h>
+#include <fcntl.h>
+#define PERMS 0666
+
+FILE *myfopen(char *name, char *mode)
+{
+  int fd;
+  FILE *fp;
+
+  if (*mode != 'r' && *mode != 'a')
+    return NULL;
+  for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
+    if ((fp->flag & (_READ | _WRITE)) == 0)
+      break;
+  if (fp >= _iob + OPEN_MAX)
+    return NULL;
+  if (*mode == 'w')
+    fd = creat(name, PERMS);
+  else if (*mode == 'a')
+  {
+    if ((fd = open(name, O_WRONLY, 0)) == -1)
+      fd = creat(name, PERMS);
+    lseek(fd, 0L, 2);
+  }
+  else
+    fd = open(name, O_RDONLY, 0);
+  if (fd == -1)
+    return NULL;
+  fp->fd = fd;
+  fp->cnt = 0;
+  fp->base = NULL;
+  fp->flag = (*mode == 'r') ? _READ : _WRITE;
+  return fp;
+}
+
+#include <stdlib.h>
+
+int _fillbuf(FILE *fp)
+{
+  int bufsize;
+  if ((fp->flag & (_READ | _EOF | _ERR)) != _READ)
+    return EOF;
+  bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZ;
+  if (fp->base == NULL)
+    if ((fp->base = (char *)malloc(bufsize)) == NULL)
+      return EOF;
+  fp->ptr = fp->base;
+  fp->cnt = read(fp->fd, fp->ptr, bufsize);
+  if (--fp->cnt < 0)
+  {
+    if (fp->cnt == -1)
+      fp->flag |= _EOF;
+    else
+      fp->flag |= _ERR;
+  }
+  return (unsigned char)*fp->ptr++;
+}
+
+FILE _iob[OPEN_MAX] = {
+    {0, (char *)0, (char *)0, _READ, 0},
+    {0, (char *)0, (char *)0, _WRITE, 1},
+    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2}};
