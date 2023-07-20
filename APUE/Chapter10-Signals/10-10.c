ce9f9968932a8274eaa8f81be79fa8fd0899bce1
Hugh He <plushugh@gmail.com>
2023-07-20 15:57:04 +0800

    normalize indent

--- APUE/Chapter10-Signals/10-10.c
+++ APUE/Chapter10-Signals/10-10.c
@@ -8,45 +8,45 @@ static jmp_buf env_alrm;
 static void
 sig_alrm2(int signo)
 {
-  longjmp(env_alrm, 1);
+	longjmp(env_alrm, 1);
 }
 
 static void
 sig_alrm1(int signo)
 {
-  /* nothing to do, just return to wake up the pause */
+	/* nothing to do, just return to wake up the pause */
 }
 
 unsigned int
 sleep1(unsigned int seconds)
 {
-  if (signal(SIGALRM, sig_alrm1) == SIG_ERR)
-    return (seconds);
-  alarm(seconds);    /* start the timer */
-  pause();           /* next caught signal wakes us up */
-  return (alarm(0)); /* turn off timer, return unslept time */
+	if (signal(SIGALRM, sig_alrm1) == SIG_ERR)
+		return (seconds);
+	alarm(seconds);	   /* start the timer */
+	pause();		   /* next caught signal wakes us up */
+	return (alarm(0)); /* turn off timer, return unslept time */
 }
 
 unsigned int
 sleep2(unsigned int seconds)
 {
-  if (signal(SIGALRM, sig_alrm2) == SIG_ERR)
-    return (seconds);
-  if (setjmp(env_alrm) == 0)
-  {
-    alarm(seconds); /* start the timer */
-    pause();        /* next caught signal wakes us up */
-  }
-  return (alarm(0)); /* turn off timer, return unslept time */
+	if (signal(SIGALRM, sig_alrm2) == SIG_ERR)
+		return (seconds);
+	if (setjmp(env_alrm) == 0)
+	{
+		alarm(seconds); /* start the timer */
+		pause();		/* next caught signal wakes us up */
+	}
+	return (alarm(0)); /* turn off timer, return unslept time */
 }
 
 int main()
 {
-  printf("sleep1(5) = %d\n", sleep1(5));
+	printf("sleep1(5) = %d\n", sleep1(5));
 
-  printf("sleep1(1) = %d\n", sleep1(1));
+	printf("sleep1(1) = %d\n", sleep1(1));
 
-  printf("sleep2(5) = %d\n", sleep2(5));
+	printf("sleep2(5) = %d\n", sleep2(5));
 
-  printf("sleep2(1) = %d\n", sleep2(1));
+	printf("sleep2(1) = %d\n", sleep2(1));
 }
